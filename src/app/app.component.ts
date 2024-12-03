090d5c1ab3be65501ab1bd94c199a240f2a4cd8e
Natthawut <pongthap_t@softsquaregroup.com>
2024-12-03 12:59:28 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-web-service';
+
+  firstName: string = '';
+  lastName: string = '';
+  nickName:string = '';
+  age:string = '';
+  birthDay:string = '';
+
+  buttonClass:string = '';
+
+  Check() {
+    if (
+      this.firstName === '' ||
+      this.lastName === '' ||
+      this.nickName === '' ||
+      this.age === '' ||
+      this.birthDay === ''
+    ) {
+      this.buttonClass = 'btn btn-danger';
+    } else {
+      this.buttonClass = 'btn btn-primary';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
